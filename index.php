9e8b170203cddb05fd812f081534e39a7eb3ce6a
unknown <kwitt@PLAP48.parsek.net>
2016-05-04 20:56:45 +0200

    Testing Git Workflow

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 
     All Emoncms code is released under the GNU Affero General Public License.
     See COPYRIGHT.txt and LICENSE.txt.
+	Testing
 
     ---------------------------------------------------------------------
     Emoncms - open source energy visualisation
